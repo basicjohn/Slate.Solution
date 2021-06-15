ba9f4877f4ca146c04bcbbbb4bf81f3c80d58b05
Usarneme <jamestcoop@gmail.com>
2021-06-15 14:31:05 -0700

    :card_file_box: adds ef migration and database info (as well as needed efcore and efcore.design package refs to server csproj)
    Co-Authored-By: John Edmondson <edmondsonj@gmail.com>
    Co-authored-by: Ahmed Ghouzlane <ahmedghouzlane@gmail.com>
    Co-authored-by: Jeremy Banka <jeremybanka@users.noreply.github.com>

---
+++ Server/Migrations/20210615212901_Initial.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Slate.Server.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    URI = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    OwnerId = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    EditorId = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    EditorExpiration = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Content = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Email = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Salt = table.Column<byte[]>(type: "longblob", nullable: true),
+                    Hash = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Boards");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
